edd849911d7c1564bf28ddaaf22f2c8bdd107482
k-sketch <kael.mazza@ucsp.edu.pe>
2021-10-03 16:15:38 -0500

    Create Extended Euclide.cpp

---
+++ Extended Euclide.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int Ext_Euclide(int a, int b, int* c, int* d){
+  int c´,d´,e;
+  if(a % b == 0){
+    *c = 0, *d = 1;
+    return b;
+  }
+  e = Ext_Euclide(b,a%b,&c´,&d´);
+  *c = d´;
+  *d = c´-(a/b)*d´;
+  return e;
+}
+
+int main() {
+  int x,y,z,f;
+  cout << "Inserte un numero: "; cin >> x;
+  cout << "Inserte un numero: "; cin >> y;
+  int e = Ext_Euclide(x,y,&z,&f);
+  cout << "gcd(" << x << "," << y << ")," << z << "," << f << " = " << e;
+}
